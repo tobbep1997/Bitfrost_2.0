d29da3ae8fcba71028a93fe1d7ddc26ad4ae4a49
Joakim <joakim.trossvik@gmail.com>
2018-11-21 19:32:36 +0100

    Assimp importer

--- BitFrost 2.0/Source.cpp
+++ BitFrost 2.0/Source.cpp
@@ -2,12 +2,24 @@
 #include <Windows.h>
 #include "3DEngine/src/3D Engine/RenderingManager.h"
 #include "3DEngine/src/Window/window.h"
+#include <assimp/cimport.h>
+#include <assimp/postprocess.h>
 
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
 	RenderingManager * renderingManager = RenderingManager::GetInstance();
 	renderingManager->Init(hInstance);
+
+	/*
+	 * BOBBY PLS READ
+	 * if .dll is missing copy from assimp/lib -->> x64/
+	 * och jag har bara includat detta i bitfrost2.0
+	 */
+
+	//const  aiScene * test = aiImportFile("C:/Users/Root/Desktop/Bitfrost_2.0/BitFrost 2.0",aiProcessPreset_TargetRealtime_MaxQuality);
+	//imp.ReadFile("")
+	
 	
 	while (renderingManager->GetWindow().isOpen())
 	{
